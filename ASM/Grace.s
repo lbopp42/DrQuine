80e443e9084a8b2ca183281c647caf51b577f6c9
Lucas BOPP <lbopp@student.42.fr>
2019-07-05 15:12:32 +0200

    Colleen Ok

---
+++ ASM/Grace.s
@@ -0,0 +1,20 @@
+section .text
+	global start
+	extern _printf
+	extern _fflush
+
+start:
+	mov rdi, msg
+	mov rsi, 10
+	mov rdx, 59
+	mov rcx, 34
+	mov r8, msg
+	call _printf
+	mov	rdi, 0
+	call _fflush
+	mov rax, 0x2000001
+	mov rdi, 0
+	syscall
+
+section .data
+	msg: db "section .text%1$c	global start%1$c	extern _printf%1$c	extern _fflush%1$c%1$c%2$cNeed it to compile%1$cstart:%1$c	mov rdi, msg%1$c	mov rsi, 10%1$c	mov rdx, 59%1$c	mov rcx, 34%1$c	mov r8, msg%1$c	call _printf%1$c	mov	rdi, 0%1$c	call _fflush%1$c	mov rax, 0x2000001%1$c	mov rdi, 0%1$c	syscall%1$c%1$csection .data%1$c	msg: db %3$c%4$s%3$c%1$c"
