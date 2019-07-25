1bdd187c1d3c79bea818cb42e3290ac8016feefa
Lucas BOPP <lbopp@student.42.fr>
2019-07-25 12:58:32 +0200

    Sully.py is seems to be ok

---
+++ Python/Sully.py
@@ -0,0 +1,15 @@
+import os
+
+def main():
+    i = 5
+    if __debug__ == False:
+        i = i - 1
+    fd = open('Sully_' + str(i) + '.py', 'w+')
+    s = "import os{0:c}{0:c}def main():{0:c}    i = {1}{0:c}    if __debug__ == False:{0:c}        i = i - 1{0:c}    fd = open('Sully_' + str(i) + '.py', 'w+'){0:c}    s = {2:c}{3:s}{2:c}{0:c}    print(s.format(0x0a, i, 0x22, s), file=fd){0:c}    fd.close(){0:c}    if i > 0:{0:c}        os.system('python3 -O Sully_' + str(i) + '.py'){0:c}{0:c}if __name__ == '__main__':{0:c}	main()"
+    print(s.format(0x0a, i, 0x22, s), file=fd)
+    fd.close()
+    if i > 0:
+        os.system('python3 -O Sully_' + str(i) + '.py')
+
+if __name__ == '__main__':
+	main()
