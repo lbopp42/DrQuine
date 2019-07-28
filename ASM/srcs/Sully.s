cfaae38d411137a3f4cb020c7f81a2c72dd886b0
Lucas BOPP <lbopp@student.42.fr>
2019-07-28 15:11:09 +0200

    Adding free in Sully.s

--- ASM/srcs/Sully.s
+++ ASM/srcs/Sully.s
@@ -11,6 +11,7 @@ section .text
 	extern _fflush
 	extern _system
 	extern _malloc
+	extern _free
 
 start:
 	push rbp
@@ -59,6 +60,8 @@ start:
 	call _sprintf
 	mov	rdi, r12
 	call _system
+	mov rdi, r12
+	call _free
 
 	;EXIT
 exit:
@@ -76,6 +79,6 @@ exit_error:
 	syscall
 
 section .data
-	msg: db "%%define MACHO_SYSCALL(nb) nb | 0x2000000%1$c%%define i %4$d%1$c%%ifndef X%1$c	%%define X i%1$c%%endif%1$c%1$csection .text%1$c	global start%1$c	extern _dprintf%1$c	extern _sprintf%1$c	extern _fflush%1$c	extern _system%1$c	extern _malloc%1$c%1$cstart:%1$c	push rbp%1$c	mov rbp, rsp%1$c	and rsp, 0xfffffffffffffff0%1$c	mov r12, i%1$c	cmp r12, 0%1$c	jle exit_error%1$c	mov rdi, rsp%1$c	lea rsi, [rel filename]%1$c	mov rdx, X%1$c	call _sprintf%1$c%1$c	;OPEN%1$c	mov rax, MACHO_SYSCALL(5)%1$c	lea rdi, [rsp]%1$c	mov rsi, 0x201%1$c	mov rdx, 777o%1$c	syscall%1$c	jc exit_error%1$c	mov [rsp + 0x8], rax%1$c%1$c	;PRINT IN FILE%1$c	mov rdi, rax%1$c	lea rsi, [rel msg]%1$c	mov rdx, 0x0a%1$c	mov rcx, 0x22%1$c	lea r8, [rel msg]%1$c	mov r9, X%1$c	call _dprintf%1$c	mov rdi, 0%1$c	call _fflush%1$c%1$c	;CLOSE%1$c	mov rdi, [rsp + 0x8]%1$c	mov rax, MACHO_SYSCALL(6)%1$c	syscall%1$c%1$c	;COMPILE + EXEC%1$c	mov rdi, 256%1$c	call _malloc%1$c	mov r12, rax%1$c	mov rdi, rax%1$c	lea rsi, [rel nasm]%1$c	mov rdx, X%1$c	call _sprintf%1$c	mov	rdi, r12%1$c	call _system%1$c%1$c	;EXIT%1$cexit:%1$c	mov rsp, rbp%1$c	pop rbp%1$c	mov rdi, rax%1$c	mov rax, MACHO_SYSCALL(1)%1$c	syscall%1$c%1$cexit_error:%1$c	mov rsp, rbp%1$c	pop rbp%1$c	mov rdi, 1%1$c	mov rax, MACHO_SYSCALL(1)%1$c	syscall%1$c%1$csection .data%1$c	msg: db %2$c%3$s%2$c, 0%1$c	filename: db %2$cSully_%%d.s%2$c, 0%1$c	nasm: db %2$cnasm -f macho64 -D X=i-1 Sully_%%1$d.s && ld -macosx_version_min 10.7.0 -lSystem -o Sully_%%1$d Sully_%%1$d.o && rm -f *.o && ./Sully_%%1$d%2$c, 0%1$c", 0
+	msg: db "%%define MACHO_SYSCALL(nb) nb | 0x2000000%1$c%%define i %4$d%1$c%%ifndef X%1$c	%%define X i%1$c%%endif%1$c%1$csection .text%1$c	global start%1$c	extern _dprintf%1$c	extern _sprintf%1$c	extern _fflush%1$c	extern _system%1$c	extern _malloc%1$c	extern _free%1$c%1$cstart:%1$c	push rbp%1$c	mov rbp, rsp%1$c	and rsp, 0xfffffffffffffff0%1$c	mov r12, i%1$c	cmp r12, 0%1$c	jle exit_error%1$c	mov rdi, rsp%1$c	lea rsi, [rel filename]%1$c	mov rdx, X%1$c	call _sprintf%1$c%1$c	;OPEN%1$c	mov rax, MACHO_SYSCALL(5)%1$c	lea rdi, [rsp]%1$c	mov rsi, 0x201%1$c	mov rdx, 777o%1$c	syscall%1$c	jc exit_error%1$c	mov [rsp + 0x8], rax%1$c%1$c	;PRINT IN FILE%1$c	mov rdi, rax%1$c	lea rsi, [rel msg]%1$c	mov rdx, 0x0a%1$c	mov rcx, 0x22%1$c	lea r8, [rel msg]%1$c	mov r9, X%1$c	call _dprintf%1$c	mov rdi, 0%1$c	call _fflush%1$c%1$c	;CLOSE%1$c	mov rdi, [rsp + 0x8]%1$c	mov rax, MACHO_SYSCALL(6)%1$c	syscall%1$c%1$c	;COMPILE + EXEC%1$c	mov rdi, 256%1$c	call _malloc%1$c	mov r12, rax%1$c	mov rdi, rax%1$c	lea rsi, [rel nasm]%1$c	mov rdx, X%1$c	call _sprintf%1$c	mov	rdi, r12%1$c	call _system%1$c	mov rdi, r12%1$c	call _free%1$c%1$c	;EXIT%1$cexit:%1$c	mov rsp, rbp%1$c	pop rbp%1$c	mov rdi, rax%1$c	mov rax, MACHO_SYSCALL(1)%1$c	syscall%1$c%1$cexit_error:%1$c	mov rsp, rbp%1$c	pop rbp%1$c	mov rdi, 1%1$c	mov rax, MACHO_SYSCALL(1)%1$c	syscall%1$c%1$csection .data%1$c	msg: db %2$c%3$s%2$c, 0%1$c	filename: db %2$cSully_%%d.s%2$c, 0%1$c	nasm: db %2$cnasm -f macho64 -D X=i-1 Sully_%%1$d.s && ld -macosx_version_min 10.7.0 -lSystem -o Sully_%%1$d Sully_%%1$d.o && rm -f *.o && ./Sully_%%1$d%2$c, 0%1$c", 0
 	filename: db "Sully_%d.s", 0
 	nasm: db "nasm -f macho64 -D X=i-1 Sully_%1$d.s && ld -macosx_version_min 10.7.0 -lSystem -o Sully_%1$d Sully_%1$d.o && rm -f *.o && ./Sully_%1$d", 0
